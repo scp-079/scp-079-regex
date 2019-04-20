3a4a8f1b409892b3481051c876e2252436e150da
Xiao <xiao@scp-079.org>
2019-04-20 03:54:18 +0000

    0.1.3

--- plugins/handlers/messages.py
+++ plugins/handlers/messages.py
@@ -32,7 +32,6 @@ logger = logging.getLogger(__name__)
 @Client.on_message(Filters.incoming & Filters.channel)
 def test(client, message):
     try:
-        logger.warning(f"{message}")
         if message.chat.id == glovar.channel_id:
             text = get_text(message)
             if text:
