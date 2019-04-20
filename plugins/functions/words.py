1dd1236d63a0ccc6cc330568067b75d495ec3bc3
Xiao <xiao@scp-079.org>
2019-04-20 15:08:08 +0000

    0.1.4

--- plugins/functions/words.py
+++ plugins/functions/words.py
@@ -161,7 +161,6 @@ def words_add(word_type, word):
             text += f"{code(old)}\n\n"
 
         text = text[:-2]
-        logger.warning(f"{text}")
         text = (f"状态：{code('未添加')}\n"
                 f"类别：{code(f'{glovar.names[word_type]}')}\n"
                 f"词组：{code(word)}\n"
