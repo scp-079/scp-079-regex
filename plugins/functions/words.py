c3464086eb94e75a7fa13ef53fcb3f3968335ed9
Xiao <xiao@scp-079.org>
2019-04-23 02:23:05 +0000

    0.1.5

--- plugins/functions/words.py
+++ plugins/functions/words.py
@@ -374,6 +374,9 @@ def words_remove_word(message: Message) -> Union[str, None]:
     uid = message.from_user.id
     text = f"管理：{user_mention(uid)}\n"
     command_list = message.command
+    if not command_list:
+        command_list = get_text(message).split(" ")
+
     # Check if the command format is correct
     if len(command_list) > 1:
         i, word_type = get_type(command_list)
