d3e3d59bb5b90b60c2a39a1d25566084f4b033a1
Xiao <xiao@scp-079.org>
2019-07-31 09:35:44 +0000

    0.3.2

--- plugins/handlers/command.py
+++ plugins/handlers/command.py
@@ -93,7 +93,7 @@ def ask_word(client: Client, message: Message):
 
 
 @Client.on_message(Filters.incoming & Filters.group & regex_group
-                   & Filters.command(glovar.list_commands, glovar.prefix))
+                   & Filters.command(["ls"], glovar.prefix))
 def list_words(client: Client, message: Message):
     try:
         cid = message.chat.id
