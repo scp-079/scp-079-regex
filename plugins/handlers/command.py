6281f73280f1fe85437325c9d40bfdd11d5f38e5
Xiao <xiao@scp-079.org>
2019-09-03 12:54:21 +0000

    - 稍加修改

--- plugins/handlers/command.py
+++ plugins/handlers/command.py
@@ -21,7 +21,7 @@ import logging
 from pyrogram import Client, Filters, Message
 
 from .. import glovar
-from ..functions.channel import share_regex_update
+from ..functions.channel import share_data, share_regex_update
 from ..functions.etc import bold, code, general_link, get_callback_data, get_command_context, get_command_type, get_text
 from ..functions.etc import message_link, thread, user_mention
 from ..functions.filters import regex_group, test_group
@@ -97,6 +97,41 @@ def ask_word(client: Client, message: Message) -> bool:
             return True
         except Exception as e:
             logger.warning(f"Ask word error: {e}", exc_info=True)
+        finally:
+            glovar.locks["regex"].release()
+
+    return False
+
+
+@Client.on_message(Filters.incoming & Filters.group & regex_group
+                   & Filters.command(["count"], glovar.prefix))
+def count_words(client: Client, message: Message) -> bool:
+    # Count words
+    if glovar.locks["regex"].acquire():
+        try:
+            cid = message.chat.id
+            mid = message.message_id
+            uid = message.from_user.id
+            receivers = []
+            for word_type in glovar.receivers:
+                receivers += glovar.receivers[word_type]
+
+            receivers = list(set(receivers))
+            share_data(
+                client=client,
+                receivers=receivers,
+                action="update",
+                action_type="count",
+                data="ask"
+            )
+            text = (f"管理：{user_mention(uid)}\n"
+                    f"操作：{code('更新计数')}\n"
+                    f"状态：{code('已请求')}\n")
+            thread(send_message, (client, cid, text, mid))
+
+            return True
+        except Exception as e:
+            logger.warning(f"Count words error: {e}", exc_info=True)
         finally:
             glovar.locks["regex"].release()
 
